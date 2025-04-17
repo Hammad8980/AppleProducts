2621dbe94d8b0e074d8135d0617acb9b6de2ec46
Hammad Mehmood <hammad.mehmood898@gmail.com>
2025-04-17 23:19:04 +0500

    fixed some .env configuration by using port, also corrected status code 500

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -30,13 +30,16 @@ export const createProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
     const { id } = req.params; // get the id from the request params
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ success: false, message: "Invalid product id" });
+    }
     try {
         await Product.findByIdAndDelete(id); // find the product by id and delete it
         res.status(200).json({ success: true, message: "Product deleted successfully" });
     }
     catch (error) {
         console.error("Error in Deleting product:", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,14 @@ import productRoutes from "./routes/route.js";
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT; // use the port from the environment variable or default to 5000
 
 app.use(express.json()); // for parsing application/json (allows us to accept JSON data in the req.body)
 
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes); // use the product routes for all requests starting with /api/products
 
-console.log(process.env.MONGO_URI);
-
-app.listen(5000, () => {
+app.listen(port, () => {
     connectDB();
-    console.log("Server is running on port http://localhost:5000");
+    console.log("Server is running on port http://localhost:"+ port);
 });
 
