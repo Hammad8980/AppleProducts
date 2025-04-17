2448353400e4cf6ede56e82c1537864385f7b271
Hammad Mehmood <hammad.mehmood898@gmail.com>
2025-04-17 22:56:03 +0500

    Added Server.js in which main app is configured, controllers and routes for cleaner code adaptation and finally product.js which contains our schema.

---
+++ backend/controller/controller.js
@@ -0,0 +1,56 @@
+import Product from "../models/Product.js";
+import mongoose from "mongoose";
+
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({}); // find all products in the database
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.error("Error in Fetching products:", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+export const createProduct = async (req, res) => {
+    const product = req.body; // user will send product data in the request body
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please fill all the fields" });
+
+    }
+    const newProduct = new Product(product);
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params; // get the id from the request params
+    try {
+        await Product.findByIdAndDelete(id); // find the product by id and delete it
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    }
+    catch (error) {
+        console.error("Error in Deleting product:", error.message);
+        res.status(404).json({ success: false, message: "Product not found" });
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params; // get the id from the request params
+    const product = req.body; // user will send product data in the request body
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ success: false, message: "Invalid product id" });
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true }); // find the product by id and update it
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.error("Error in Updating product:", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+app.use(express.json()); // for parsing application/json (allows us to accept JSON data in the req.body)
+
+app.use("/api/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
     connectDB();
     console.log("Server is running on port http://localhost:5000");
-})
+});
 
